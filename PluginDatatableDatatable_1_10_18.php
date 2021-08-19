9c90b4016c40dc954c08cc92bc6b1af968310ebc
Claes Östlund <claes@stenbergit.se>
2021-08-19 16:14:43 +0200

    1.4.5

--- PluginDatatableDatatable_1_10_18.php
+++ PluginDatatableDatatable_1_10_18.php
@@ -35,6 +35,10 @@ class PluginDatatableDatatable_1_10_18{
      */
     $element[] = wfDocument::createHtmlElement('style', ".dt-buttons button{font-size:10px}");
     /**
+     * Position processing card just below the table.
+     */
+    $element[] = wfDocument::createHtmlElement('style', ".dataTables_processing{top:1% !important;}");
+    /**
      * 
      */
     wfDocument::renderElement($element);
@@ -58,6 +62,13 @@ class PluginDatatableDatatable_1_10_18{
      * 
      */
     $element[] = wfDocument::createHtmlElement('script', "var datatable_$id; $(document).ready(function(){ datatable_$id = $('#$id').DataTable($json); });");
+    /**
+     * Position processing card just below the table.
+     */
+    $element[] = wfDocument::createHtmlElement('script', "$('.dataTables_processing').addClass('bg-info');");
+    /*
+     *
+     */
     wfDocument::renderElement($element);
   }
   /**
